fcf4793cf0435c3fdaa00326a4c898bf1e6367ff
elbertribeiro <elbertprofissional@gmail.com>
2022-04-06 20:10:04 -0300

    Sugestão de alteração

--- src/main/java/com/mariaalicia/pessoasapi/entidade/Pessoa.java
+++ src/main/java/com/mariaalicia/pessoasapi/entidade/Pessoa.java
@@ -14,32 +14,35 @@ public class Pessoa {
 	private int idade;
 	private String cargo;
 
-	
-	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
 	public String getCargo() {
 		return cargo;
 	}
+
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
-	
-
 }
